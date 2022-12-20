61dd983aee56f910282cba0083b259610b4d533e
lychen0618 <412369718@qq.com>
2022-12-20 22:31:44 +0800

    Add initial debug support by prettify printing.

---
+++ src/pretty/pretty.go
@@ -0,0 +1,64 @@
+package pretty
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"time"
+)
+
+// Retrieve the verbosity level from an environment variable
+func getVerbosity() int {
+	v := os.Getenv("VERBOSE")
+	level := 0
+	if v != "" {
+		var err error
+		level, err = strconv.Atoi(v)
+		if err != nil {
+			log.Fatalf("Invalid verbosity %v", v)
+		}
+	}
+	return level
+}
+
+type LogTopic string
+
+const (
+	Client  LogTopic = "CLNT"
+	Commit  LogTopic = "CMIT"
+	Drop    LogTopic = "DROP"
+	Error   LogTopic = "ERRO"
+	Info    LogTopic = "INFO"
+	Leader  LogTopic = "LEAD"
+	Log     LogTopic = "LOG1"
+	Log2    LogTopic = "LOG2"
+	Persist LogTopic = "PERS"
+	Snap    LogTopic = "SNAP"
+	Term    LogTopic = "TERM"
+	Test    LogTopic = "TEST"
+	Timer   LogTopic = "TIMR"
+	Trace   LogTopic = "TRCE"
+	Vote    LogTopic = "VOTE"
+	Warn    LogTopic = "WARN"
+)
+
+var debugStart time.Time
+var debugVerbosity int
+
+func Init() {
+	debugVerbosity = getVerbosity()
+	debugStart = time.Now()
+
+	log.SetFlags(log.Flags() &^ (log.Ldate | log.Ltime))
+}
+
+func Debug(topic LogTopic, format string, a ...interface{}) {
+	if debugVerbosity >= 1 {
+		time := time.Since(debugStart).Microseconds()
+		time /= 100
+		prefix := fmt.Sprintf("%06d %v ", time, string(topic))
+		format = prefix + format
+		log.Printf(format, a...)
+	}
+}
